611b9dd0bf444e011b7ba018d2295900ce55d6f8
Mo Beigi <me@mobeigi.com>
2020-06-27 00:26:41 +1000

    Add continue to blog button

--- app/src/shared/components/SocialButtonGroup/SocialButton/styled.js
+++ app/src/shared/components/SocialButtonGroup/SocialButton/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import COLOURS from '../../../constants/Colors';
 
 export const StyledSocialButton = styled.div`
+    display: inline-block;
     color: ${COLOURS.grey};
     &:hover {
         color: ${COLOURS.white};
