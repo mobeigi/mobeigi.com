c66a834902758dbaf173dbd092cefc8ea1ac532a
Mo Beigi <me@mobeigi.com>
2020-06-27 20:14:09 +1000

    Add title name changes with react hooks

--- app/src/TradePage/TradePage.tsx
+++ app/src/TradePage/TradePage.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import COMMON from '../shared/constants/Common';
 import { StyledIFrameContainer, StyledIFrame } from './styled';
 
-const TradePage = () => (
-  <StyledIFrameContainer>
-    <StyledIFrame
-      title="google-spreadsheet-trades"
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsBCcRwfXoi9lHSseTZExNeM-Mw0gAVw7vBsz7TzLfPuZLfcWOaB865blkhvYRbcZb3m0WKLInoOL4/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
-    />
-  </StyledIFrameContainer>
-);
+const TradePage = () => {
+  useEffect(() => {
+    document.title = `${COMMON.WEBSITE.titlePrefix}Trades`;
+  });
+
+  return (
+    <StyledIFrameContainer>
+      <StyledIFrame
+        title="google-spreadsheet-trades"
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsBCcRwfXoi9lHSseTZExNeM-Mw0gAVw7vBsz7TzLfPuZLfcWOaB865blkhvYRbcZb3m0WKLInoOL4/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
+      />
+    </StyledIFrameContainer>
+  );
+};
 
 export default TradePage;
