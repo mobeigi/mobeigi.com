c1c0c816ecdab395448cb4635d226794590b11c2
Mo Beigi <me@mobeigi.com>
2020-06-28 20:52:39 +1000

    Add Google Analytics to every page via ReactGA

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Switch,
   Route,
+  useLocation,
 } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import Header from '../shared/components/Header';
 import Homepage from '../Homepage';
 import NotFoundPage from '../NotFoundPage';
 import TradePage from '../TradePage';
 import Footer from '../shared/components/Footer';
+import COMMON from '../shared/constants/Common';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../shared/assets/bootstrap-themes/slate.css';
@@ -16,22 +19,34 @@ import './SlateOverrides.css';
 import './reset.css';
 import './App.css';
 
-const App : React.FunctionComponent = () => (
-  <div className="App">
-    <main>
-      <Header />
-      <br />
-      <Switch>
-        {/* Pages */}
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/trades" component={TradePage} />
+// Init Google Analytics
+ReactGA.initialize(COMMON.ANALYTICS.gaTrackingId);
 
-        {/* Catch All */}
-        <Route component={NotFoundPage} />
-      </Switch>
-    </main>
-    <Footer />
-  </div>
-);
+const App : React.FunctionComponent = () => {
+  const location = useLocation();
+
+  // Fired on every route change
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
+  return (
+    <div className="App">
+      <main>
+        <Header />
+        <br />
+        <Switch>
+          {/* Pages */}
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/trades" component={TradePage} />
+
+          {/* Catch All */}
+          <Route component={NotFoundPage} />
+        </Switch>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   rootElement,
 );
