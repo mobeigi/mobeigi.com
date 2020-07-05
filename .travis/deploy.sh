6e7416504100cfa3fad80ba7f87a41d14e530503
Mo Beigi <me@mobeigi.com>
2020-07-05 18:10:00 +1000

    Check for service startup failure and remove npm package-lock

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,6 +12,8 @@ ssh git@$IP sudo systemctl stop mobeigi-express
 rsync -avz --delete -e ssh $TRAVIS_BUILD_DIR/server/ git@$IP:$SERVER_DEPLOY_DIR
 ssh git@$IP /bin/bash << EOF
     cd $SERVER_DEPLOY_DIR
+    /bin/cp -rf ../server_private_files/* .
     yarn install
     sudo systemctl start mobeigi-express
+    systemctl is-active mobeigi-express
 EOF
