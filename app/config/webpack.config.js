f4ffe625e5a10f13eb6a4a611f65a0df2fdbcee1
Mo Beigi <me@mobeigi.com>
2020-07-20 23:54:34 +1000

    Add maximumFileSizeToCacheInBytes to Workbox to avoid build errors

--- app/config/webpack.config.js
+++ app/config/webpack.config.js
@@ -610,6 +610,7 @@ module.exports = function(webpackEnv) {
         new WorkboxWebpackPlugin.GenerateSW({
           clientsClaim: true,
           exclude: [/\.map$/, /asset-manifest\.json$/],
+          maximumFileSizeToCacheInBytes: 3000000,
           navigateFallback: paths.publicUrlOrPath + 'index.html',
           navigateFallbackDenylist: [
             // Exclude URLs starting with /_, as they're likely an API call
