1411af8f3109b8babf57521988355d3ab750c001
Mo Beigi <me@mobeigi.com>
2020-10-03 14:02:27 +1000

    Remove old trade spreadsheet until automated IB trade scrape solution is added

--- app/src/TradePage/TradePage.tsx
+++ app/src/TradePage/TradePage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 import COMMON from '../shared/constants/Common';
-import { StyledIFrameContainer, StyledIFrame } from './styled';
 
 const TradePage = () => (
   <>
@@ -13,12 +12,9 @@ const TradePage = () => (
       </title>
       <link rel="canonical" href={`${COMMON.WEBSITE.baseURL}/trades`} />
     </Helmet>
-    <StyledIFrameContainer>
-      <StyledIFrame
-        title="google-spreadsheet-trades"
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQsBCcRwfXoi9lHSseTZExNeM-Mw0gAVw7vBsz7TzLfPuZLfcWOaB865blkhvYRbcZb3m0WKLInoOL4/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
-      />
-    </StyledIFrameContainer>
+    <p>
+      Work in progress. Check back later!
+    </p>
   </>
 );
 
