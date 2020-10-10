58df0a5b1a084f1efedbea543694b4e1f7c8b08f
Mo Beigi <me@mobeigi.com>
2020-10-10 17:04:20 +1100

    Explicitly specify JSON content type in response

--- server/routes/resume.js
+++ server/routes/resume.js
@@ -8,27 +8,27 @@ var authKeyError = { error: "Provided 'authKey' is not authorised."};
 
 router.post('/auth', function(req, res, next) {
   if (req.body.authKey === undefined) {
-    return res.status(422).send(JSON.stringify(paramError)).end();
+    return res.status(422).contentType('json').send(JSON.stringify(paramError)).end();
   }
   var key = req.body.authKey;
   if (validKeyArray.indexOf(key) > -1) {
-    return res.status(204).end();
+    return res.status(204).contentType('json').end();
   }
   else {
-    return res.status(401).send(JSON.stringify(authKeyError));
+    return res.status(401).contentType('json').send(JSON.stringify(authKeyError));
   }
 });
 
 router.post('/download', function(req, res, next) {
   if (req.body.authKey === undefined) {
-    return res.status(422).send(JSON.stringify(paramError)).end();
+    return res.status(422).contentType('json').send(JSON.stringify(paramError)).end();
   }
   var key = req.body.authKey;
   if (validKeyArray.indexOf(key) > -1) {
     return res.download('private/resume/resume.pdf');
   }
   else {
-    return res.status(401).send(JSON.stringify(authKeyError));
+    return res.status(401).contentType('json').send(JSON.stringify(authKeyError));
   }
 });
 
