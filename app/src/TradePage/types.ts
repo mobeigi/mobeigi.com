388e22315028982d9e7b8a714d60633533e93a07
Mo Beigi <me@mobeigi.com>
2020-10-10 14:48:13 +1100

    Add automated IB trades for trade page

---
+++ app/src/TradePage/types.ts
@@ -0,0 +1,19 @@
+export type Trade = {
+    tradeID: number,
+    symbol: string,
+    strike: number,
+    expiry: string,
+    putCall: string,
+    dateTime: Date,
+    quantity: number,
+    tradePrice: number,
+    ibCommission: number,
+    orderType: string,
+    currency: string,
+}
+
+export type State = {
+    trades: Trade[],
+    whenGenerated: Date | null,
+    error: boolean,
+}
