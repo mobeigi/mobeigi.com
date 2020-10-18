7e609ca421d9b9a38bebfa6015bbf9c85f1aab0a
Mo Beigi <me@mobeigi.com>
2020-10-18 18:12:39 +1100

    Add StockTwits to stock page

---
+++ app/src/TradePage/StockTwitsWidget/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StockTwitsIFrame = styled.iframe`
+    width: 800px;
+    height: 400px;
+    overflow: hidden,
+`;
+
+export default { StockTwitsIFrame };
--- app/src/TradePage/TradePage.tsx
+++ app/src/TradePage/TradePage.tsx
@@ -4,16 +4,17 @@ import Axios from 'axios';
 import moment from 'moment';
 import 'moment-timezone';
 import { MoonLoader } from 'react-spinners';
+
 import COLORS from '../shared/constants/Colors';
 import { LoaderCss, FadeInDiv } from '../shared/styles/common';
 import TargetAwareLink from '../shared/utils/TargetAwareLink';
-
 import { StyledTable } from './styled';
 import type { OpenPosition, State, Trade } from './types';
 import {
   calcTotalPrice, getPutOrCallFullText, displayFractionAsPercentage, getOpenPositionTotalPrice,
 } from './utils';
 import COMMON from '../shared/constants/Common';
+import StockTwitsWidget from './StockTwitsWidget';
 
 const TradePage = () => {
   const [state, setState] = React.useState<State>({
@@ -75,6 +76,23 @@ const TradePage = () => {
       });
   }, []);
 
+  const lastUpdated = (
+    <p>
+      <strong>Last Updated: </strong>
+      {state.whenGenerated && new Intl.DateTimeFormat('en-GB', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      }).format(state.whenGenerated)}
+      {` ${moment.tz(state.timezone).zoneName()}`}
+    </p>
+  );
+
+  const preloadedStockTwitsWidget = <StockTwitsWidget />;
+
   return (
     <>
       <Helmet>
@@ -99,170 +117,187 @@ const TradePage = () => {
       )}
       {!state.loading && !state.error && (
         <FadeInDiv>
-          <p>
-            {'Showing '}
-            <strong>{state.trades.length}</strong>
-            {' trades over the last '}
-            <strong>365</strong>
-            {' days.'}
-          </p>
-          <p>
-            <strong>Last Updated: </strong>
-            {state.whenGenerated && new Intl.DateTimeFormat('en-GB', {
-              year: 'numeric',
-              month: 'long',
-              day: '2-digit',
-              hour: 'numeric',
-              minute: 'numeric',
-              second: 'numeric',
-            }).format(state.whenGenerated)}
-            {` ${moment.tz(state.timezone).zoneName()}`}
-          </p>
+          <div>
+            <h2>
+              StockTwits
+              (
+              <TargetAwareLink
+                to="https://stocktwits.com/mobeigi"
+                title="StockTwits (@mobeigi)"
+                aria-label="StockTwits (@mobeigi)"
+                rel="external nofollow"
+              >
+                @mobeigi
+              </TargetAwareLink>
+              )
+            </h2>
+            <br />
+            {preloadedStockTwitsWidget}
+          </div>
           <br />
-          <h2>Open Positions</h2>
           <br />
-          <StyledTable className="table table-hover table-active">
-            <thead>
-              <tr>
-                <th>Quantity</th>
-                <th>Symbol</th>
-                <th>Strike</th>
-                <th>Put / Call</th>
-                <th>Expiry</th>
-                <th>Last Price Per Unit</th>
-                <th>Total Price</th>
-                <th>Currency</th>
-                <th>Weight</th>
-              </tr>
-            </thead>
-            <tbody>
-              {state.openPositions
-                .map((openPosition) => (
-                  <tr key={openPosition.symbol}>
-                    <td>{openPosition.position}</td>
-                    <td>
-                      <TargetAwareLink
-                        to={`https://finance.yahoo.com/quote/${openPosition.symbol.split(' ')[0]}`}
-                        title={`${openPosition.symbol.split(' ')[0]} (${openPosition.description})`}
-                        aria-label={`${openPosition.symbol.split(' ')[0]} (${openPosition.description})`}
-                        rel="external nofollow"
-                      >
-                        {openPosition.symbol.split(' ')[0]}
-                      </TargetAwareLink>
-                    </td>
-                    <td>{openPosition?.strike?.toFixed(0)}</td>
-                    <td>{getPutOrCallFullText({ putCall: openPosition.putCall })}</td>
-                    <td>
-                      {openPosition.expiry && new Intl.DateTimeFormat('en-GB', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit',
-                      }).format(openPosition.expiry)}
+          <div>
+            <h2>Open Positions</h2>
+            <br />
+            {'Holding '}
+            <strong>{state.openPositions.length}</strong>
+            {' positions.'}
+            {lastUpdated}
+            <br />
+            <StyledTable className="table table-hover table-active">
+              <thead>
+                <tr>
+                  <th>Quantity</th>
+                  <th>Symbol</th>
+                  <th>Strike</th>
+                  <th>Put / Call</th>
+                  <th>Expiry</th>
+                  <th>Last Price Per Unit</th>
+                  <th>Total Price</th>
+                  <th>Currency</th>
+                  <th>Weight</th>
+                </tr>
+              </thead>
+              <tbody>
+                {state.openPositions
+                  .map((openPosition) => (
+                    <tr key={openPosition.symbol}>
+                      <td>{openPosition.position}</td>
+                      <td>
+                        <TargetAwareLink
+                          to={`https://finance.yahoo.com/quote/${openPosition.symbol.split(' ')[0]}`}
+                          title={`${openPosition.symbol.split(' ')[0]} (${openPosition.description})`}
+                          aria-label={`${openPosition.symbol.split(' ')[0]} (${openPosition.description})`}
+                          rel="external nofollow"
+                        >
+                          {openPosition.symbol.split(' ')[0]}
+                        </TargetAwareLink>
+                      </td>
+                      <td>{openPosition?.strike?.toFixed(0)}</td>
+                      <td>{getPutOrCallFullText({ putCall: openPosition.putCall })}</td>
+                      <td>
+                        {openPosition.expiry && new Intl.DateTimeFormat('en-GB', {
+                          year: 'numeric',
+                          month: 'long',
+                          day: '2-digit',
+                        }).format(openPosition.expiry)}
 
-                    </td>
-                    <td>{openPosition.markPrice.toFixed(2)}</td>
-                    <td>
-                      {calcTotalPrice({
-                        pricePerShare: openPosition.markPrice,
-                        quantity: openPosition.position,
-                        isOptionContract: !!openPosition.putCall,
-                      })
-                        .toFixed(2)}
-                    </td>
-                    <td>{openPosition.currency}</td>
-                    <td>
-                      {displayFractionAsPercentage(
-                        {
-                          fraction:
+                      </td>
+                      <td>{openPosition.markPrice.toFixed(2)}</td>
+                      <td>
+                        {calcTotalPrice({
+                          pricePerShare: openPosition.markPrice,
+                          quantity: openPosition.position,
+                          isOptionContract: !!openPosition.putCall,
+                        })
+                          .toFixed(2)}
+                      </td>
+                      <td>{openPosition.currency}</td>
+                      <td>
+                        {displayFractionAsPercentage(
+                          {
+                            fraction:
                           (openPosition.position * openPosition.markPrice)
                           / getOpenPositionTotalPrice({ openPositions: state.openPositions }),
-                        },
-                      )}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </StyledTable>
+                          },
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </StyledTable>
+          </div>
           <br />
           <br />
-          <h2>Trades</h2>
-          <br />
-          <StyledTable className="table table-hover table-active">
-            <thead>
-              <tr>
-                <th>
-                  Date
-                  <br />
-                  (
-                  {moment.tz(state.timezone).zoneName()}
-                  )
-                </th>
-                <th>Buy / Sell</th>
-                <th>Quantity</th>
-                <th>Symbol</th>
-                <th>Strike</th>
-                <th>Put / Call</th>
-                <th>Expiry</th>
-                <th>Price Per Unit</th>
-                <th>Total Price</th>
-                <th>Commission</th>
-                <th>Currency</th>
-              </tr>
-            </thead>
-            <tbody>
-              {state.trades
-                .map((trade) => (
-                  <tr key={trade.tradeID}>
-                    <td>
-                      {new Intl.DateTimeFormat('en-GB', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit',
-                        hour: 'numeric',
-                        minute: 'numeric',
-                        second: 'numeric',
-                      }).format(trade.dateTime)}
-                    </td>
-                    <td>
-                      {trade.quantity >= 0 ? <span className="badge badge-success">BUY</span>
-                        : <span className="badge badge-danger">SELL</span> }
-                    </td>
-                    <td>{Math.abs(trade.quantity)}</td>
-                    <td>
-                      <TargetAwareLink
-                          to={`https://finance.yahoo.com/quote/${trade.symbol.split(' ')[0]}`}
-                          title={`${trade.symbol.split(' ')[0]} (${trade.description})`}
-                          aria-label={`${trade.symbol.split(' ')[0]} (${trade.description})`}
-                          rel="external nofollow"
-                      >
-                        {trade.symbol.split(' ')[0]}
-                      </TargetAwareLink>
-                    </td>
-                    <td>{trade?.strike?.toFixed(0)}</td>
-                    <td>{getPutOrCallFullText({ putCall: trade.putCall })}</td>
-                    <td>
-                      {trade.expiry && new Intl.DateTimeFormat('en-GB', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit',
-                      }).format(trade.expiry)}
+          <div>
+            <h2>Trades</h2>
+            <br />
+            <p>
+              {'Showing '}
+              <strong>{state.trades.length}</strong>
+              {' trades over the last '}
+              <strong>365</strong>
+              {' days.'}
+            </p>
+            {lastUpdated}
+            <br />
+            <StyledTable className="table table-hover table-active">
+              <thead>
+                <tr>
+                  <th>
+                    Date
+                    <br />
+                    (
+                    {moment.tz(state.timezone).zoneName()}
+                    )
+                  </th>
+                  <th>Buy / Sell</th>
+                  <th>Quantity</th>
+                  <th>Symbol</th>
+                  <th>Strike</th>
+                  <th>Put / Call</th>
+                  <th>Expiry</th>
+                  <th>Price Per Unit</th>
+                  <th>Total Price</th>
+                  <th>Commission</th>
+                  <th>Currency</th>
+                </tr>
+              </thead>
+              <tbody>
+                {state.trades
+                  .map((trade) => (
+                    <tr key={trade.tradeID}>
+                      <td>
+                        {new Intl.DateTimeFormat('en-GB', {
+                          year: 'numeric',
+                          month: 'long',
+                          day: '2-digit',
+                          hour: 'numeric',
+                          minute: 'numeric',
+                          second: 'numeric',
+                        }).format(trade.dateTime)}
+                      </td>
+                      <td>
+                        {trade.quantity >= 0 ? <span className="badge badge-success">BUY</span>
+                          : <span className="badge badge-danger">SELL</span> }
+                      </td>
+                      <td>{Math.abs(trade.quantity)}</td>
+                      <td>
+                        <TargetAwareLink
+                            to={`https://finance.yahoo.com/quote/${trade.symbol.split(' ')[0]}`}
+                            title={`${trade.symbol.split(' ')[0]} (${trade.description})`}
+                            aria-label={`${trade.symbol.split(' ')[0]} (${trade.description})`}
+                            rel="external nofollow"
+                        >
+                          {trade.symbol.split(' ')[0]}
+                        </TargetAwareLink>
+                      </td>
+                      <td>{trade?.strike?.toFixed(0)}</td>
+                      <td>{getPutOrCallFullText({ putCall: trade.putCall })}</td>
+                      <td>
+                        {trade.expiry && new Intl.DateTimeFormat('en-GB', {
+                          year: 'numeric',
+                          month: 'long',
+                          day: '2-digit',
+                        }).format(trade.expiry)}
 
-                    </td>
-                    <td>{trade.tradePrice.toFixed(2)}</td>
-                    <td>
-                      {calcTotalPrice({
-                        pricePerShare: trade.tradePrice,
-                        quantity: trade.quantity,
-                        isOptionContract: !!trade.putCall,
-                      })
-                        .toFixed(2)}
-                    </td>
-                    <td>{trade.ibCommission.toFixed(2)}</td>
-                    <td>{trade.currency}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </StyledTable>
+                      </td>
+                      <td>{trade.tradePrice.toFixed(2)}</td>
+                      <td>
+                        {calcTotalPrice({
+                          pricePerShare: trade.tradePrice,
+                          quantity: trade.quantity,
+                          isOptionContract: !!trade.putCall,
+                        })
+                          .toFixed(2)}
+                      </td>
+                      <td>{trade.ibCommission.toFixed(2)}</td>
+                      <td>{trade.currency}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </StyledTable>
+          </div>
         </FadeInDiv>
       )}
     </>
