b5179c8435bdb4578051ec463dece9c4667400cd
Mo Beigi <me@mobeigi.com>
2020-11-15 18:17:03 +1100

    Add overview page content

---
+++ app/src/TradePage/Overview/utils.ts
@@ -0,0 +1,52 @@
+import moment from 'moment';
+import {
+  GetTradesInRangeProps,
+  FilterStockTradesProps,
+  FilterOptionsTradesProps,
+  GetNetDepositWithdrawalInRangeProps,
+  GetEquitySummaryInBaseForDayProps,
+} from './utils.types';
+
+export const getCurrentFinancialYearStartDate = () => {
+  const startOfFinancialYear = moment().month(6).startOf('month');
+  if (startOfFinancialYear > moment()) {
+    startOfFinancialYear.subtract(1, 'year');
+  }
+  return startOfFinancialYear.toDate();
+};
+
+export const getTradesInRange = ({ trades, from, to }: GetTradesInRangeProps) => trades.filter(
+  (trade) => trade.dateTime > from && trade.dateTime < to,
+);
+
+export const filterStockTrades = ({ trades }: FilterStockTradesProps) => trades.filter(
+  (trade) => trade.putCall === null,
+);
+
+export const filterOptionTrades = ({ trades }: FilterOptionsTradesProps) => trades.filter(
+  (trade) => trade.putCall !== null,
+);
+
+export const getEquitySummaryInBaseForDay = (
+  { equitySummaryInBase, date }: GetEquitySummaryInBaseForDayProps,
+) => (
+  equitySummaryInBase.find(
+    (equitySummaryInBaseEntry) => moment(equitySummaryInBaseEntry.reportDate).isSame(moment(date)),
+  ));
+
+export const getNetDepositWithdrawalInRange = (
+  { depositsWithdrawals, from, to }: GetNetDepositWithdrawalInRangeProps,
+) => depositsWithdrawals
+  .filter(
+    (depositsWithdrawal) => depositsWithdrawal.dateTime > from && depositsWithdrawal.dateTime < to,
+  )
+  .reduce((accumulator, current) => accumulator + current.amount, 0);
+
+export default {
+  getCurrentFinancialYearStartDate,
+  getTradesInRange,
+  filterStockTrades,
+  filterOptionTrades,
+  getEquitySummaryInBaseForDay,
+  getNetDepositWithdrawalInRange,
+};
