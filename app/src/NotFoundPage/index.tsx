6b832235901f5c4189ee171edf624892cb95080e
Mo Beigi <me@mobeigi.com>
2021-07-03 16:36:22 +1000

    Use react-helmet-async instead of react-hemlet

--- app/src/NotFoundPage/index.tsx
+++ app/src/NotFoundPage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 
 import COMMON from '../shared/constants/Common';
 import { MonospacedParagraph } from '../shared/styles/common';
