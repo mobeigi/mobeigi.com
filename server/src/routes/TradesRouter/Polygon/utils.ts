9b3fe82b58ce84e5591e0bff6f62238e2987eac1
Mo Beigi <me@mobeigi.com>
2021-09-26 13:10:36 +1000

    Adjust daily open close backfill range dates and storage time

--- server/src/routes/TradesRouter/Polygon/utils.ts
+++ server/src/routes/TradesRouter/Polygon/utils.ts
@@ -10,7 +10,7 @@ import type {
 import { SPY_SYMBOL, POLYGON_API_DATE_FORMAT, NEW_YORK_TIMEZONE } from './constants';
 
 export const createNoDataDailyOpenClose = ({ date }: CreateNoDataDailyOpenCloseProps): DBPolygonDailyOpenCloseModel => {
-  const from_date = parseDateStringWithNewYorkTz({ date });
+  const from_date = convertDate({ date });
   return {
     from_date,
     symbol: SPY_SYMBOL,
@@ -22,7 +22,7 @@ export const createDBModelFromPolygonDailyOpenClose = ({
   polygonDailyOpenClose,
   date,
 }: CreateDBModelFromPolygonDailyOpenCloseProps): DBPolygonDailyOpenCloseModel => {
-  const from_date = parseDateStringWithNewYorkTz({ date });
+  const from_date = convertDate({ date });
   return {
     from_date,
     symbol: polygonDailyOpenClose.symbol,
@@ -53,6 +53,7 @@ export const createMarketDailyOpenCloseEntryFromDBModel = ({
   };
 };
 
-const parseDateStringWithNewYorkTz = ({ date }: ParseDateStringWithNewYorkTzProps) => {
-  return moment.tz(date, POLYGON_API_DATE_FORMAT, NEW_YORK_TIMEZONE).toDate();
+const convertDate = ({ date }: ParseDateStringWithNewYorkTzProps) => {
+  // Data is accurate only for the end of the day
+  return moment.tz(date, POLYGON_API_DATE_FORMAT, NEW_YORK_TIMEZONE).endOf('day').toDate();
 };
