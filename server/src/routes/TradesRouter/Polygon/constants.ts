922c8019f44460d3b9f435750f7e16d10199c463
Mo Beigi <me@mobeigi.com>
2021-09-26 13:10:36 +1000

    Adjust daily open close backfill range dates and storage time

--- server/src/routes/TradesRouter/Polygon/constants.ts
+++ server/src/routes/TradesRouter/Polygon/constants.ts
@@ -4,4 +4,4 @@ export const POLYGON_API_MAX_REQUESTS_PER_MINUTE = 5; // free tier vibes
 export const POLYGON_API_DATE_FORMAT = 'YYYY-MM-DD';
 export const UTC_TIMEZONE = 'Etc/GMT';
 export const NEW_YORK_TIMEZONE = 'America/New_York';
-export const BACKFILL_START_DATE = '01 Jun 2021 00:00:00 GMT';
+export const BACKFILL_START_DATE = '01 Jun 2021 23:59:59 EDT';
