5499db138f1b795efdb6a591d07d852376494ab9
Mo Beigi <me@mobeigi.com>
2024-08-22 18:02:05 +1000

    Add default metadata for root layout

---
+++ app/src/constants/app.ts
@@ -0,0 +1,5 @@
+import getNextEnv from "@/utils/getNextEnv";
+
+export const BASE_URL = getNextEnv('NEXT_PUBLIC_BASE_URL');
+export const SITE_TITLE = getNextEnv('NEXT_PUBLIC_SITE_TITLE');
+export const TAGLINE = getNextEnv('NEXT_PUBLIC_TAGLINE');
