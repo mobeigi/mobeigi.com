ffaad160fe1b17c3f7a0d47804c18d4b5ac58802
Mo Beigi <me@mobeigi.com>
2024-08-22 18:05:29 +1000

    Use roboto font

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Roboto } from "next/font/google";
 import { BASE_URL, SITE_TITLE } from "@/constants/app";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"],});
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
@@ -24,7 +24,7 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
