3bc6168759944697abc38ef156e6cdd8c3402a72
Mo Beigi <me@mobeigi.com>
2024-08-29 00:41:08 +1000

    Add stub for resume

---
+++ app/src/app/resume/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Resume',
+  description: 'TODO',
+  keywords: ['TODO'],
+};
+
+const Resume = () => <div>Resume</div>;
+
+export default Resume;
