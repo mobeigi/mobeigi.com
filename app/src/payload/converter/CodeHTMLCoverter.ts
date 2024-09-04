d53be2a99a7eb264446052432259c999d1b855b0
Mo Beigi <me@mobeigi.com>
2024-09-04 13:20:24 +1000

    Make code html converter handle auto case

--- app/src/payload/converter/CodeHTMLCoverter.ts
+++ app/src/payload/converter/CodeHTMLCoverter.ts
@@ -11,16 +11,21 @@ const CodeBlockHTMLConverter: HTMLConverter<CodeBlockNode> = {
   converter: ({ node }) => {
     const fields = node.fields;
     const code = fields.code;
+
+    const auto = fields.language === 'auto';
+
     const language = fields.language || 'plaintext';
+    const highlightOptions = {
+      language,
+    };
 
     // TODO: Commonise this so it can be used elsewhere outside of Payload too
     // Apply server-side syntax highlighting with Highlight.js
-    const highlightedCode = hljs.highlight(code, {
-      language: language,
-    }).value;
+    const highlightResult = auto ? hljs.highlightAuto(code) : hljs.highlight(code, highlightOptions);
+    const hljsClasses = ['hljs', highlightResult.language].filter(Boolean);
 
     // Return the highlighted code block
-    return `<pre><code class="language-${language} hljs">${highlightedCode}</code></pre>`;
+    return `<pre><code class="${hljsClasses.join(' ')}">${highlightResult.value}</code></pre>`;
   },
   nodeTypes: ['block'],
 };
