743071925a558f16fa13b92c4d9a95c2c95fa3e0
Mo Beigi <me@mobeigi.com>
2024-09-14 22:06:19 +1000

    Prevent email / ip address from being read by non-authd users

--- app/src/payload/access/authenticated.ts
+++ app/src/payload/access/authenticated.ts
@@ -1,8 +1,6 @@
 import type { AccessArgs } from 'payload';
 
-import type { User } from '@/payload-types';
-
-type isAuthenticated = (args: AccessArgs<User>) => boolean;
+type isAuthenticated = (args: AccessArgs<any>) => boolean;
 
 export const authenticated: isAuthenticated = ({ req: { user } }) => {
   return Boolean(user);
