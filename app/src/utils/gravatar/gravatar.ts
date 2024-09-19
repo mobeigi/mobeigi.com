224bd248f139a2a0e68ec4ae6c91c40346fce323
Mo Beigi <me@mobeigi.com>
2024-09-20 02:06:45 +1000

    Tweak default rating for gravatar avatars

--- app/src/utils/gravatar/gravatar.ts
+++ app/src/utils/gravatar/gravatar.ts
@@ -1,11 +1,16 @@
 import crypto from 'crypto';
 import { GetGravatarAvatarUrlProps } from './types';
 
-export const getGravatarAvatarUrl = async ({ email, size = 80 }: GetGravatarAvatarUrlProps): Promise<string | null> => {
+export const getGravatarAvatarUrl = async ({
+  email,
+  size = 80,
+  rating = 'pg',
+}: GetGravatarAvatarUrlProps): Promise<string | null> => {
   const sha256EmailHash = crypto.createHash('sha256').update(email.toLowerCase()).digest('hex');
   const queryParams: Record<string, string> = {
     d: '404',
     s: size.toString(),
+    r: rating,
   };
   const queryString = new URLSearchParams(queryParams).toString();
   const url = `https://gravatar.com/avatar/${sha256EmailHash}?${queryString}`;
