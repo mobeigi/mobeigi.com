b04df2721222da764b6ec325a8b396b52e6a9152
Mo Beigi <me@mobeigi.com>
2024-09-21 06:26:38 +1000

    Use only 1 h1 on contact page

--- app/src/containers/ContactPage/ContactPage.tsx
+++ app/src/containers/ContactPage/ContactPage.tsx
@@ -1,18 +1,19 @@
 export const ContactPage = () => (
   <div>
-    <h1>Email Me</h1>
+    <h1>Contact</h1>
+    <h2>Email Me</h2>
     <p>
       If you would like to contact me, please send an email to <a href="mailto:me@mobeigi.com">me@mobeigi.com</a>.
     </p>
     <p>
       This is the fastest way to reach me, and I typically respond within <strong>24</strong> hours.
     </p>
-    <h1>Need some privacy?</h1>
+    <h2>Need some privacy?</h2>
     <p>I have a PGP Public key available if you prefer to encrypt your emails to me.</p>
     <p>
       <a href="/sec/mo-beigi-pgp-pub.asc">Mo Beigi&apos;s PGP Public Key</a>
     </p>
-    <h1>Networks</h1>
+    <h2>Networks</h2>
     <p>You can also reach me on other networks where I&apos;m active.</p>
     <p>
       Check the <a href="#footer">footer</a> of this page for a list of networks.
