e90caec1a6c3f86543146d108527cf46b8a05a2e
Mo Beigi <me@mobeigi.com>
2024-09-22 00:06:11 +1000

    Link to categories page from blog page

--- app/src/containers/BlogPage/BlogPage.tsx
+++ app/src/containers/BlogPage/BlogPage.tsx
@@ -1,12 +1,16 @@
 import { BlogPageProps } from './types';
 import { BlogPageContainer, BlogSummaryWrapper } from './styled';
 import BlogSummary from '@/components/BlogSummary';
+import Link from 'next/link';
 
 export const BlogPage = ({ blogPostMetas }: BlogPageProps) => {
   return (
     <BlogPageContainer>
       <h1>Blog</h1>
-      <p>Explore my thoughts and insights through the blog posts below</p>
+      <p>
+        Explore my thoughts and insights in the blog posts below, or browse by topics on the{' '}
+        <Link href="/blog/category">categories</Link> page.
+      </p>
       {blogPostMetas.length > 0 ? (
         <p>
           Showing <strong>{blogPostMetas.length}</strong> {blogPostMetas.length === 1 ? 'post' : 'posts'} in this blog.
