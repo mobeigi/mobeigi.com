e42518f0f34f2ba799831199934a6d770dfaf60a
Mo Beigi <me@mobeigi.com>
2024-09-23 12:12:16 +1000

    Make icon/text in footer highlight together

--- app/src/components/FooterContent/BuildInfo/index.tsx
+++ app/src/components/FooterContent/BuildInfo/index.tsx
@@ -3,31 +3,32 @@
 // TODO: Remove above workaround when @svgr/webpack bug is resolved. This component does not need to be a client side component.
 // Github link: https://github.com/vercel/next.js/issues/69545
 
-// TODO: Once above bug is resolved, flatten this into FooterContent.tsx parent
-
-import { IconWrapper } from '@/styles/icon';
-import { BuildInfo } from './styled';
+import { BuildInfoSpan, IconAndTextContainer, StyledA, CustomIconWrapper as IconWrapper } from './styled';
 import GithubSvg from '@/assets/icons/boxicons/bxl-github.svg';
 import GitCommitSvg from '@/assets/icons/boxicons/bx-git-commit.svg';
 import { COMMIT_HASH } from '@/constants/build';
 
-export const BuildInfoComponent = () => (
+export const BuildInfo = () => (
   <>
-    <BuildInfo>
-      <IconWrapper>
-        <GitCommitSvg />
-      </IconWrapper>
-      <span>
-        <a href={`https://github.com/mobeigi/mobeigi.com/commit/${COMMIT_HASH}`}>{COMMIT_HASH}</a>
-      </span>
-    </BuildInfo>
-    <BuildInfo>
-      <IconWrapper>
-        <GithubSvg />
-      </IconWrapper>
-      <span>
-        <a href="https://github.com/mobeigi/mobeigi.com">mobeigi/mobeigi.com</a>
-      </span>
-    </BuildInfo>
+    <BuildInfoSpan>
+      <StyledA href={`https://github.com/mobeigi/mobeigi.com/commit/${COMMIT_HASH}`}>
+        <IconAndTextContainer>
+          <IconWrapper>
+            <GitCommitSvg />
+          </IconWrapper>
+          <span>{COMMIT_HASH}</span>
+        </IconAndTextContainer>
+      </StyledA>
+    </BuildInfoSpan>
+    <BuildInfoSpan>
+      <StyledA href="https://github.com/mobeigi/mobeigi.com">
+        <IconAndTextContainer>
+          <IconWrapper>
+            <GithubSvg />
+          </IconWrapper>
+          <span>mobeigi/mobeigi.com</span>
+        </IconAndTextContainer>
+      </StyledA>
+    </BuildInfoSpan>
   </>
 );
