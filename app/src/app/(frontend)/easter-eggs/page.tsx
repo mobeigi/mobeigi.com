5d3f3c0f55b7c6ac7532742de22c849aec6c254b
Mo Beigi <me@mobeigi.com>
2024-09-25 19:54:22 +1000

    Add easter eggs page

---
+++ app/src/app/(frontend)/easter-eggs/page.tsx
@@ -0,0 +1,44 @@
+import EasterEggsPage from '@/containers/EasterEggsPage';
+import { Metadata } from 'next';
+import { generateBreadcrumbs as generateParentBreadcrumbs } from '../page';
+import { BreadcrumbList, ListItem, WithContext } from 'schema-dts';
+import { appendItem } from '@/utils/seo/breadCrumbList';
+import { getLastItemId } from '@/utils/seo/listItem';
+import { joinUrl } from '@/utils/url';
+import Breadcrumbs from '@/components/Breadcrumbs';
+
+export const metadata: Metadata = {
+  title: 'Easter Eggs',
+  description: "Here are some fun Easter Eggs i've added to my website, I hope you enjoy them!",
+};
+
+export const generateBreadcrumbs = (): WithContext<BreadcrumbList> | null => {
+  let breadcrumbList = generateParentBreadcrumbs();
+  const lastItemId = getLastItemId(breadcrumbList.itemListElement as ListItem[]);
+  if (!lastItemId) {
+    return null;
+  }
+  appendItem({
+    breadcrumbList: breadcrumbList,
+    id: joinUrl([lastItemId, 'easter-eggs']),
+    name: 'Easter Eggs',
+  });
+  return breadcrumbList;
+};
+
+const EasterEggs = () => {
+  const breadcrumbs = generateBreadcrumbs();
+  return (
+    <div>
+      {breadcrumbs && (
+        <>
+          <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbs) }} />
+          <Breadcrumbs breadcrumbList={breadcrumbs} />
+        </>
+      )}
+      <EasterEggsPage />
+    </div>
+  );
+};
+
+export default EasterEggs;
