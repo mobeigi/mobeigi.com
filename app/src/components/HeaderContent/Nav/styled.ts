74b4ac1582bc69bda72b04b1435b7f94b674c1bf
Mo Beigi <me@mobeigi.com>
2024-09-25 22:30:26 +1000

    Add mobile nav for low vw, make border included in header height, allow overflow in header distribution

--- app/src/components/HeaderContent/Nav/styled.ts
+++ app/src/components/HeaderContent/Nav/styled.ts
@@ -3,6 +3,47 @@
 import styled from 'styled-components';
 import Link from 'next/link';
 
+export const DesktopNav = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile.maxWidth}px) {
+    display: none;
+  }
+`;
+export const MobileNav = styled.div`
+  display: none;
+
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  nav {
+    flex-direction: column;
+  }
+
+  svg {
+    font-size: 1.5em;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile.maxWidth}px) {
+    display: flex;
+  }
+`;
+
+export const DropdownMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.frame.background};
+  padding: 1em 0;
+
+  top: calc(var(--header-height));
+  left: 0;
+  position: absolute;
+  width: 100%;
+`;
+
 export const ContainerNav = styled.nav`
   display: flex;
   gap: 1em;
