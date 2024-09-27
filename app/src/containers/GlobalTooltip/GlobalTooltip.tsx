fa8ddea645e80e7ee6202a3b548a9afa5d5ef563
Mo Beigi <me@mobeigi.com>
2024-09-27 11:37:01 +1000

    Add theme support for tooltip

--- app/src/containers/GlobalTooltip/GlobalTooltip.tsx
+++ app/src/containers/GlobalTooltip/GlobalTooltip.tsx
@@ -1,10 +1,16 @@
+'use client';
+
+import { useTheme } from 'styled-components';
 import { GlobalTooltipWrapper, StyledTooltip } from './styled';
 
 // Provides a top level tooltip that can be reused by the entire app
 // It is best practice to store one copy of this tooltip and reuse it for performance and to avoid various DOM render bugs
-// TODO: Tooltip needs to support themes other than just dark
-export const GlobalTooltip = () => (
-  <GlobalTooltipWrapper>
-    <StyledTooltip id={`base-tooltip`} place="top" variant="dark" opacity={0.9} />
-  </GlobalTooltipWrapper>
-);
+export const GlobalTooltip = () => {
+  const { currentTheme } = useTheme();
+
+  return (
+    <GlobalTooltipWrapper>
+      <StyledTooltip id={`base-tooltip`} place="top" opacity={0.9} className={`rt-theme-${currentTheme}`} />
+    </GlobalTooltipWrapper>
+  );
+};
