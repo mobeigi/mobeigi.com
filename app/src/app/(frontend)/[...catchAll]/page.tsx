81603332f68252ae7c89cea17998842cee066b64
Mo Beigi <me@mobeigi.com>
2024-09-28 15:59:38 +1000

    Upgrade to payload 3.0.0-beta.109, next 15.0.0-canary.172 and react 19.0.0-rc-3edc000d-20240926

--- app/src/app/(frontend)/[...catchAll]/page.tsx
+++ app/src/app/(frontend)/[...catchAll]/page.tsx
@@ -3,14 +3,16 @@ import { joinUrl } from '@/utils/url';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
-export const generateMetadata = async ({ params }: { params: { catchAll: string[] } }): Promise<Metadata> => {
-  await payloadRedirect({ currentUrl: joinUrl(['/', ...params.catchAll]) });
+export const generateMetadata = async ({ params }: { params: Promise<{ catchAll: string[] }> }): Promise<Metadata> => {
+  const resolvedParams = await params;
+  await payloadRedirect({ currentUrl: joinUrl(['/', ...resolvedParams.catchAll]) });
   notFound();
 };
 
-const CatchAllHandler = async ({ params }: { params: { catchAll: string[] } }) => {
-  // Any route that isn't handled by other routes will end up here
-  await payloadRedirect({ currentUrl: joinUrl(['/', ...params.catchAll]) });
+// Any route that isn't handled by other routes will end up here
+const CatchAllHandler = async ({ params }: { params: Promise<{ catchAll: string[] }> }) => {
+  const resolvedParams = await params;
+  await payloadRedirect({ currentUrl: joinUrl(['/', ...resolvedParams.catchAll]) });
   notFound();
 };
 
