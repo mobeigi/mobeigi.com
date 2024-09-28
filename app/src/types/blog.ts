86ae4df8c8e84496a178e5e124e38ba7f8f2182b
Mo Beigi <me@mobeigi.com>
2024-09-28 17:29:33 +1000

    Remove unused breadcrumbs type

--- app/src/types/blog.ts
+++ app/src/types/blog.ts
@@ -1,11 +1,5 @@
 import { SerializedEditorState } from 'lexical';
 
-export type Breadcrumb = {
-  title: string;
-  slug: string;
-  url: string;
-};
-
 export type Category = {
   title: string;
   description: string;
@@ -21,7 +15,6 @@ export type BlogPostPostMeta = {
   url: string;
   views: number;
   category: Category;
-  breadcrumbs: Breadcrumb[]; // TODO: can this be removed?
   commentsEnabled: boolean;
 };
 
