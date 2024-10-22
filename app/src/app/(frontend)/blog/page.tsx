4a09cac0381d9eb104f0578603e30394dfcb6814
Mo Beigi <me@mobeigi.com>
2024-10-23 00:15:58 +1100

    Add caching to data fetch for blog page

--- app/src/app/(frontend)/blog/page.tsx
+++ app/src/app/(frontend)/blog/page.tsx
@@ -7,55 +7,74 @@ import { BlogPostMeta, BlogPostRelatedMeta } from '@/types/blog';
 import { sortBlogPostMetaByPublishedAtDate } from '@/utils/blog/post';
 import Breadcrumbs from '@/components/Breadcrumbs';
 import { generateBreadcrumbs } from './breadcrumbs';
+import { unstable_cache_safe } from '@/utils/next';
 
 export const revalidate = 900;
 
+/**
+ * Data fetching
+ */
+
+const getAllBlogPostMetas = unstable_cache_safe(
+  async () => {
+    const payload = await getPayloadHMR({
+      config,
+    });
+
+    const posts = await payload.find({
+      collection: 'posts',
+      where: { _status: { equals: 'published' } },
+      depth: 1,
+      limit: 0,
+      pagination: false,
+    });
+
+    return (
+      await Promise.all(
+        posts.docs.map(async (post) => {
+          const postMeta = mapPostToPostMeta(post);
+          if (!postMeta) {
+            console.warn('postMeta should not be null.');
+            return null;
+          }
+          const commentCount = await payload.count({
+            collection: 'comments',
+            where: { post: { equals: post.id } },
+          });
+
+          const relatedMeta: BlogPostRelatedMeta = {
+            commentCount: commentCount.totalDocs,
+          };
+
+          const blogPostMeta: BlogPostMeta = {
+            post: postMeta,
+            related: relatedMeta,
+          };
+
+          return blogPostMeta;
+        }),
+      )
+    )
+      .filter((obj) => obj !== null)
+      .sort(sortBlogPostMetaByPublishedAtDate);
+  },
+  [`get-all-blog-post-metas`],
+  { revalidate: revalidate },
+);
+
+/**
+ * Metadata
+ */
 export const metadata: Metadata = {
   title: 'Blog',
   description: "Explore Mo's blog for insights on programming, technology, and other topics of interest.",
 };
 
+/**
+ * Handler
+ */
 const BlogPageHandler = async () => {
-  const payload = await getPayloadHMR({
-    config,
-  });
-
-  const posts = await payload.find({
-    collection: 'posts',
-    where: { _status: { equals: 'published' } },
-    depth: 1,
-    limit: 0,
-    pagination: false,
-  });
-
-  const blogPostMetas = (
-    await Promise.all(
-      posts.docs.map(async (post) => {
-        const postMeta = mapPostToPostMeta(post);
-        if (!postMeta) {
-          console.warn('postMeta should not be null.');
-          return null;
-        }
-        const commentCount = await payload.count({
-          collection: 'comments',
-          where: { post: { equals: post.id } },
-        });
-
-        const relatedMeta: BlogPostRelatedMeta = {
-          commentCount: commentCount.totalDocs,
-        };
-
-        const blogPostMeta: BlogPostMeta = {
-          post: postMeta,
-          related: relatedMeta,
-        };
-
-        return blogPostMeta;
-      }),
-    )
-  )
-    .filter((obj) => obj !== null)
-    .sort(sortBlogPostMetaByPublishedAtDate);
+  const blogPostMetas = await getAllBlogPostMetas();
 
   const breadcrumbs = generateBreadcrumbs();
 
