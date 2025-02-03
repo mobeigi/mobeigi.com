379f1538719298d1c949390f9f55a85a25ad35f6
Mo Beigi <me@mobeigi.com>
2025-02-03 16:27:16 +1100

    Use proper CSS comments in string templates in styled.ts files

--- app/src/components/BaseTooltip/styled.ts
+++ app/src/components/BaseTooltip/styled.ts
@@ -5,12 +5,15 @@ import { Tooltip } from 'react-tooltip';
 import zIndex from '@/styles/zindex';
 
 export const BaseTooltipWrapper = styled.div`
-  // Set desired z-index
+  /* Set desired z-index */
   z-index: ${zIndex.tooltip};
 
-  // Make tooltips position fixed
-  // Has to be applied to wrapper div around tooltip
-  // This also fixes some flickering issues when focusing from one tooltip directly to another one that is wide on mobile
+  /* 
+   * Make tooltips position fixed
+   * Has to be applied to wrapper div around tooltip
+   * This also fixes some flickering issues when focusing 
+   * from one tooltip directly to another one that is wide on mobile
+   */
   position: fixed;
 
   & .rt-theme-dark {
@@ -25,7 +28,7 @@ export const BaseTooltipWrapper = styled.div`
 `;
 
 export const StyledTooltip = styled(Tooltip)`
-  // This ensures the tooltip contents are never bigger than the viewport itself
+  /* This ensures the tooltip contents are never bigger than the viewport itself */
   max-width: 90vw;
   max-height: 90vh;
 `;
--- app/src/components/HeaderContent/DarkModeSwitchContainer/styled.ts
+++ app/src/components/HeaderContent/DarkModeSwitchContainer/styled.ts
@@ -6,7 +6,7 @@ export const DarkModeSwitchWrapper = styled.div`
   display: flex;
   justify-content: center;
 
-  // Prevent button from triggering clicks as we will programatially call it
+  /* Prevent button from triggering clicks as we will programatially call it */
   pointer-events: none;
 
   button:focus,
