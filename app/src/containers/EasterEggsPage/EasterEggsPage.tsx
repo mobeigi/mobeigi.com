0e01428ba4b68d83aa8c11260923e2cf1e093660
Mo Beigi <me@mobeigi.com>
2026-05-22 02:52:49 +1000

    Add leaked .env and http header easter eggs

--- app/src/containers/EasterEggsPage/EasterEggsPage.tsx
+++ app/src/containers/EasterEggsPage/EasterEggsPage.tsx
@@ -7,7 +7,7 @@ export const EasterEggsPage = () => (
       I&apos;ve added various Easter Eggs to my website for fun. You can find out about <strong>some</strong> of them
       below.
     </p>
-    <h2>Robot in my robots.txt</h2>
+    <h2>Robot in robots.txt</h2>
     <p>Hmm, how did that get in there?</p>
     <p>
       <Link href="/robots.txt">robots.txt</Link>
@@ -16,6 +16,16 @@ export const EasterEggsPage = () => (
     <p>I&apos;ll trust you not to spy on me. 🙏</p>
     <p>
       <Link href="/sec/mo-beigi-pgp-priv.asc">mo-beigi-pgp-priv.asc</Link>
+    </p>
+    <h2>Leaked .env</h2>
+    <p>Oops, forgot to exclude this from the build.</p>
+    <p>
+      <Link href="/.env">.env</Link>
+    </p>
+    <h2>HTTP Headers</h2>
+    <p>Worth the extra 27 bytes per response.</p>
+    <p>
+      <code>curl -I https://mobeigi.com</code>
     </p>
   </section>
 );
